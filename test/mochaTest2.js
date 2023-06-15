82350d2e5d62a3adb27b701c8813048d10b2bc8b
trevharri <trevor.harri94@gmail.com>
2023-06-15 09:51:47 +0200

    added seconded test and configured the tests to run in parallel

---
+++ test/mochaTest2.js
@@ -0,0 +1,36 @@
+const { Builder, By, Key } = require('selenium-webdriver')
+// const assert = require('assert')
+var should = require("chai").should()
+
+
+describe("Add another todo tests", function(){
+
+    it("Successfully adds another todo to the application.", async function(){
+        let driver = await new Builder().forBrowser("safari").build()
+
+        //navigate to our application
+        await driver.get("https://lambdatest.github.io/sample-todo-app/")
+
+        //add a todo
+        await driver.findElement(By.id("sampletodotext")).sendKeys("Learn Selenium", Key.RETURN)
+
+        //assert
+        let todoText = await driver
+            .findElement(By.xpath("//li[last()]/span"))
+            .getText()
+            .then(function(value){
+                return value
+        })
+
+        //chai should
+        todoText.should.equal("Learn Selenium")
+
+        await driver.quit()
+    });
+
+
+});
+
+
+
+
