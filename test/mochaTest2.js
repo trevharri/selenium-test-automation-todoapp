028ee2cd58938778585f92b0bea6766108b8618f
trevharri <trevor.harri94@gmail.com>
2023-06-15 10:25:58 +0200

    added mochawesome to generate HTML reports

--- test/mochaTest2.js
+++ test/mochaTest2.js
@@ -28,6 +28,30 @@ describe("Add another todo tests", function(){
         await driver.quit()
     });
 
+    it("Adding a new test for reporting.", async function(){
+        let driver = await new Builder().forBrowser("safari").build()
+
+        //navigate to our application
+        await driver.get("https://lambdatest.github.io/sample-todo-app/")
+
+        //add a todo
+        await driver.findElement(By.id("sampletodotext")).sendKeys("Learn Selenium", Key.RETURN)
+
+        //assert
+        let todoText = await driver
+            .findElement(By.xpath("//li[last()]/span"))
+            .getText()
+            .then(function(value){
+                return value
+        })
+
+        //chai should
+        todoText.should.equal("Learn JavaScript")
+
+        await driver.quit()
+    });
+
+
 
 });
 
